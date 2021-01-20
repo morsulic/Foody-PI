6b964d1d4c39a5c92af2d880f0fc4dae82a1b8c0
ktrn <katarina.lusicic@gmail.com>
2021-01-21 00:02:54 +0100

    added menu, modified login, signup, menu

---
+++ src/views/mainMenu.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="mainMenu">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form class="menu-form">
+              <h1 style="padding: 45px;">
+                <span style='color: #000'>F</span>
+                <span style='color: #FF7043'>oo</span>
+                <span style='color: #000'>dy</span>
+              </h1>
+              
+            <button type="button" class="btn btn-primary menu-button first" >
+              New weekly plan
+            </button><br>
+            <button type="button" class="btn btn-primary menu-button second">
+            View recipes
+            </button><br>
+            <button type="button" class="btn btn-primary menu-button third">
+            Add new food
+            </button><br>
+            <p style="padding-bottom:140px;"></p>
+          </form>
+
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  @import '@/assets/scss/main.scss';
+
+  button.menu-button {
+  background-color: $green !important;
+  border: 0 !important;
+  font-size: 14px;
+  font-weight: bold;
+  height: 40px;
+  margin-top: 10px;
+  outline: 0;
+  text-transform: uppercase;
+  width: 70% !important;
+}
+
+form.menu-form {
+  background-image: url("../assets/img/mainMenu.png");
+  background-position: right top;
+  background-color: #fff;
+  border-radius: 5px;
+  margin: 50px auto 0 auto;
+  padding: 25px 15px;
+  text-align: center;
+  width: 100%;
+}
+
+button.first{
+    background-color:#8BC34A !important;
+}
+
+button.second{
+    background-color:#9DCC66 !important;
+}
+
+button.third{
+    background-color:#B4DA88 !important;
+}
+
+</style>
+
