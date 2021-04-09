cdc4dca1abaf2f3a8582ca4ec58a2535f0503d37
Matija <morsulic11@gmail.com>
2021-04-09 16:24:04 +0200

    manja izmjena#2

--- src/components/rowCard.vue
+++ src/components/rowCard.vue
@@ -5,6 +5,16 @@
       <td>{{ info.row[0].quantity }}</td>
       <td>{{ info.row[0].measUnit }}</td>
     </tr>
+    <tr>
+      <td>{{ info.row[1].ingredient }}</td>
+      <td>{{ info.row[1].quantity }}</td>
+      <td>{{ info.row[1].measUnit }}</td>
+    </tr>
+    <tr>
+      <td>{{ info.row[2].ingredient }}</td>
+      <td>{{ info.row[2].quantity }}</td>
+      <td>{{ info.row[2].measUnit }}</td>
+    </tr>
   </tbody>
 </template>
 
